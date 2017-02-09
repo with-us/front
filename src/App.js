f89b2aa3bcda1188d312e4e358a733a810461803
遠藤孝志 <takashi@endoutakashinoMacBook-Pro.local>
2017-02-09 12:18:00 +0900

    mobxによるステート管理まで

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import mobx from 'mobx'
+const {observable, computed, autorun} = mobx;
 import './App.css';
 var Firebase = require("firebase");
+import {enableLogging} from 'mobx-logger';
+enableLogging()
 
 var ref = new Firebase("https://tks-blog.firebaseio.com");
 var authData = ref.getAuth();
@@ -24,8 +28,21 @@ if (authData) {
   });
 }
 
+class Todo {
+    id = Math.random();
+    @observable title;
+    @observable finished = false;
+    constructor(title) {
+        this.title = title;
+    }
+}
+
+let todo = new Todo('name')
+
+var disposer = autorun(() => console.log(todo.title));
 
 
+todo.title = 'hello'
 
 class App extends Component {
   render() {
